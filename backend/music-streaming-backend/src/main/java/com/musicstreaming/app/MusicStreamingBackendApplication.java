8358eb5760bfb45b8b87e7ee85b2df66760707e2
Srinath Udhayakumar <srinathudhayakumar17@gmail.com>
2026-02-09 12:08:27 +0530

    Initializing Repository

---
+++ backend/music-streaming-backend/src/main/java/com/musicstreaming/app/MusicStreamingBackendApplication.java
@@ -0,0 +1,16 @@
+package com.musicstreaming.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+
+@SpringBootApplication(
+		exclude = { UserDetailsServiceAutoConfiguration.class }
+)
+public class MusicStreamingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStreamingBackendApplication.class, args);
+	}
+
+}
